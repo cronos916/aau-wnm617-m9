0bed8d97f89c4671deaac9e85dd41e91b5f9d5a5
KJ Sampat <kj@oaklandinteractive.com>
2016-07-20 15:35:53 -0400

    added jqueryui dialog

--- js/main.js
+++ js/main.js
@@ -38,6 +38,53 @@ $( function() {
     }
   });
 
+  $( "#dialog" ).dialog({
+    autoOpen: false,
+    width: 'auto',
+    maxWidth: 600,
+    height: 'auto',
+    modal: true,
+    fluid: true, //new option
+    resizable: false,
+    draggable: false,
+    dialogClass: "alert"
+  });
 
+  $( "#opener" ).on( "click", function() {
+    $( "#dialog" ).dialog( "open" );
+  });
+
+  $( "#dialog i" ).on( "click", function() {
+    $( "#dialog" ).dialog( "close" );
+  });
+
+  // catch dialog if opened within a viewport smaller than the dialog width
+  $(document).on("dialogopen", ".ui-dialog", function (event, ui) {
+    fluidDialog();
+  });
+
+  // This should make the dialog box responsive.
+  function fluidDialog() {
+    var $visible = $(".ui-dialog:visible");
+    // each open dialog
+    $visible.each(function () {
+      var $this = $(this);
+      var dialog = $this.find(".ui-dialog-content").data("ui-dialog");
+      // if fluid option == true
+      if (dialog.options.fluid) {
+        var wWidth = $(window).width();
+        // check window width against dialog width
+        if (wWidth < (parseInt(dialog.options.maxWidth) + 50))  {
+          // keep dialog from filling entire screen
+          $this.css("max-width", "90%");
+        } else {
+          // fix maxWidth bug
+          $this.css("max-width", dialog.options.maxWidth + "px");
+        }
+        //reposition dialog
+        dialog.option("position", dialog.options.position);
+      }
+    });
+  }
 } );
 
